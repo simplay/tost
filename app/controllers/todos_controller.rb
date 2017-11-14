b8dccfeb8710736e6f795897cfd1d3b7f4a3efc2
simplay <silent.simplay@gmail.com>
2017-11-14 18:53:14 +0100

    Implement basic todo actions

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,57 @@
+class TodosController < ApplicationController
+
+  def show
+    @todo = Todo.find params[:id]
+  end
+
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new todo_params
+    if @todo.save
+      redirect_to new_todo_path
+    else
+      redirect_to new_todo_path
+    end
+  end
+
+  def edit
+    @todo = Todo.find params[:id]
+  end
+
+  def update
+    @todo = Todo.find params[:id]
+    if @todo.update_attributes(todo_params)
+      redirect_to edit_todo_path(@todo)
+    else
+      redirect_to todos_path
+    end
+  end
+
+  def destroy
+    @todo = Todo.find params[:id]
+    if @todo.destroy
+
+    end
+    redirect_to todos_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(
+      :title,
+      :description,
+      :due,
+      :done
+    )
+  end
+
+end
+
