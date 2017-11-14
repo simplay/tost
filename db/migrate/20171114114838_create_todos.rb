c3ab1153fdf5e09719b04d5915c861185007a734
simplay <silent.simplay@gmail.com>
2017-11-14 12:54:04 +0100

    Introduce Todo model

---
+++ db/migrate/20171114114838_create_todos.rb
@@ -0,0 +1,12 @@
+class CreateTodos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.text :description
+      t.date :due
+      t.boolean :done, default: false
+
+      t.timestamps
+    end
+  end
+end
